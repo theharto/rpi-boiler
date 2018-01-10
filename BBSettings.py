817823fd81b41f0cf908cae65d173c425d21297c
dave <dave@dave.net>
2018-01-10 22:28:57 +0000

    backup 01/10/18 22:28:57

--- BBSettings.py
+++ BBSettings.py
@@ -1,6 +1,11 @@
-import json
-import threading
+import json, threading, logging
 import collections # NB python3.6 uses ordered dictionary, so won't need this in future
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+
+SETTINGS_FNAME = "settings.json"
 
 class BBSettings:
 	def __init__(self):
@@ -8,30 +13,36 @@ class BBSettings:
 		self.__lock = threading.Lock()
 		
 		# Set default values
-		self.__settings['client_refresh'] = 30
-		self.__settings['thermometer_refresh'] = 300
-		self.__settings['controller_tick'] = 60
-		self.__settings['hysteresis'] = 0.5
-		self.__settings['min_switching'] = 60 #'rest' ?
-		self.__settings['debug_mode'] = 1
-		self.__settings['test_mode'] =  1
-		self.__settings['relay_gpio'] = 15
-		self.__settings['led_gpio'] = 21
-		#self.__settings['meta_bools'] = ['debug_mode', 'test_mode']
-		#self.__settings['meta_restart'] = ['relay_gpio', 'led_gpio']
-		
-		# Read json from settings file, if it exists
-		try:
-			with open('settings.json', 'r') as f:
-				data = json.load(f)
-				print("loaded json")
-				print(data)
-		except IOError:
-			print("settings.json file not found, using default values")
-			
-	def __str__(self):
 		with self.__lock:
-			return str(self.settings)
+			self.__settings['client_refresh'] = 30
+			self.__settings['thermometer_refresh'] = 300
+			self.__settings['controller_tick'] = 60
+			self.__settings['hysteresis'] = 0.5
+			self.__settings['min_switching'] = 60 #'rest' ?
+			self.__settings['debug_mode'] = 1
+			self.__settings['test_mode'] =  1
+			self.__settings['relay_gpio'] = 15
+			self.__settings['led_gpio'] = 21
+			#self.__settings['meta_bools'] = ['debug_mode', 'test_mode']
+			#self.__settings['meta_restart'] = ['relay_gpio', 'led_gpio']
+		
+		self.__load_settings()
+	
+	def __save_settings(self):
+		try:
+			with self.__lock, open('settings.json', 'w') as f:
+				f.write(json.dumps(self.__settings, indent=4))
+			log.info("Settings saved to %s", SETTINGS_FNAME)
+		except IOError:
+			log.error("Error saving settings to %s", SETTINGS_FNAME)
+	
+	def __load_settings(self):
+		try:
+			with self.__lock, open(SETTINGS_FNAME, 'r') as f:
+				self.__settings = json.load(f, object_pairs_hook=collections.OrderedDict)
+			log.info("Settings loaded from %s", SETTINGS_FNAME)
+		except IOError:
+			log.warning("Error loading settings from %s", SETTINGS_FNAME)
 	
 	def get_json(self):
 		with self.__lock:
@@ -43,23 +54,23 @@ class BBSettings:
 				# type is important, so cast to original type
 				t = type(self.__settings[key])
 				value = t(value)
-			print("settings.set:", key, value)
 			self.__settings[key] = value
-			
-			# save to settings file
-			with open('settings.json', 'w') as f:
-				f.write(json.dumps(self.__settings))
-		
+		log.info("settings.set %s:%s", key, value)
+		self.__save_settings()
+	
 	def get(self, key):
 		with self.__lock:
 			return self.__settings[key]
-		
+	
 	def remove(self, key):
 		with self.__lock:
 			if key in self.__settings:
 				self.__settings.pop(key, None)
-				
+		self.__save_settings()
 
 if __name__ == "__main__":
 	s = BBSettings()
 	print(s.get_json())
+	s.set("newstr", "bs")
+	s.set("newint", 10)
+	
